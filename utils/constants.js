9b12decc8fc3f62254913b0f63066ab1f2695248
Pawel Gozdz <pawel.gozdz.web.dev@gmail.com>
2022-01-01 14:23:31 +0100

    check master

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_PATH = 'http://dev-api-stroje.agencja-impuls.atthost24.pl';
+export const BASE_PATH = 'http://api-stroje.agencja-impuls.atthost24.pl';
 export const TOKEN = 'accessToken';
 
 export const menu = [{
